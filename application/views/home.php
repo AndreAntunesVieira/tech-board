91f3fcd6e65d14859db2b8f120487bfa155caa7e
Andre Antunes Vieira <andreantunesv@gmail.com>
2021-02-22 13:57:43 -0300

    Preparando view login

---
+++ application/views/home.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Nosso Kanban</title>
+
+	<link href="/public/styles/style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+
+<div id="container">
+	<h1>Bem-vindo ao nosso Kanban</h1>
+
+	<div id="body">
+		Código aqui
+	</div>
+</div>
+
+</body>
+</html>
